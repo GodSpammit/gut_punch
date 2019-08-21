517c8c38e5e77be751ee62a278051ece92cbed91
GodSpammit <54344054+GodSpammit@users.noreply.github.com>
2019-08-20 21:32:46 -0700

    Adding columns

--- controller.js
+++ controller.js
@@ -1,3 +1,7 @@
 $(function() {
     console.log( "ready!" );
+    column1 = ["lazy", "stupid", "insecure", "idiotic", "slimy", "slutty", "smelly", "pompous", "communist", "dicknose", "pie-eating", "racist", "elitist", "white trash", "drug-loving", "butterface", "tone deaf", "ugly", "creepy"];
+    column2 = ["douche", "ass", "turd", "rectum", "butt", "cock", "shit", "crotch", "bitch", "turd", "prick", "slut", "taint", "fuck", "dick", "boner", "shart", "nut", "sphincter"];
+    column3 = ["pilot", "canoe", "pirate", "hammer", "knob", "box", "jockey", "nazi", "waffle", "goblin", "blossum", "biscuit", "clown", "socket", "monster", "hound", "dragon", "balloon"];
+    
 });
